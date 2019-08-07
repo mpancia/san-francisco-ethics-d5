8f9985a368ecb1f08cb683169dce5777f033c907
Matthew Pancia <msp@themsp.org>
2019-08-07 09:00:42 -0700

    Update schema and plan.

--- R/plan.R
+++ R/plan.R
@@ -13,35 +13,35 @@ con <- neo4j_api$new(url = DB_URL, user = DB_USER, password = DB_PASSWORD)
 
 raw_data_plan <- drake_plan(
   data_key = RSocrata::read.socrata("https://data.sfgov.org/resource/wygs-cc76.json"),
-  data_key_csv = write_csv(data_key, file_out("data/data_key.csv")),
+  data_key_csv = write_csv(data_key, here(file_out("data/data_key.csv"))),
   raw_data = RSocrata::read.socrata(paste("https://data.sfgov.org/resource/q66q-d2tr.json?$where=",
                                           "date_extract_y(Thru_date) >= 2018 AND",
                                           "Entity_Cd = 'IND' AND",
                                           "(Filer_Id = '1407918' OR",
                                           "Filer_Id = '1408942')")),
-  raw_data_csv = write_csv(raw_data, file_out("data/raw_data.csv"))
+  raw_data_csv = write_csv(raw_data, here(file_out("data/raw_data.csv"))),
 )
 
 extract_entities_plan <- drake_plan(
   filers_df = extract_filers(raw_data),
-  filers_csv = write_csv(filers_df, file_out("data/filers.csv")),
+  filers_csv = write_csv(filers_df, here(file_out("data/filers.csv"))),
   donors_df = extract_donors(raw_data),
-  donors_csv = write_csv(donors_df, file_out("data/donors.csv")),
+  donors_csv = write_csv(donors_df, here(file_out("data/donors.csv"))),
   donations_df = extract_donations(raw_data),
-  donations_csv = write_csv(donations_df, file_out("data/donations.csv")),
+  donations_csv = write_csv(donations_df, here(file_out("data/donations.csv"))),
   employers_df = extract_employers(raw_data),
-  employers_csv = write_csv(employers_df, file_out("data/employers.csv")),
+  employers_csv = write_csv(employers_df, here(file_out("data/employers.csv"))),
 )
 
 extract_relationships_plan <- drake_plan(
   employment_df = extract_employment(raw_data),
-  employment_csv = write_csv(employment_df, file_out("data/employment.csv")),
+  employment_csv = write_csv(employment_df, here(file_out("data/employment.csv"))),
   donation_donors_df = extract_donation_donors(raw_data),
-  donation_donors_csv = write_csv(donation_donors_df, file_out("data/donation_donors.csv")),
+  donation_donors_csv = write_csv(donation_donors_df, here(file_out("data/donation_donors.csv"))),
   donation_filers_df = extract_donation_filers(raw_data),
-  donation_filers_csv = write_csv(donation_filers_df, file_out("data/donation_filers.csv")),
+  donation_filers_csv = write_csv(donation_filers_df, here(file_out("data/donation_filers.csv"))),
   donor_zips_df = extract_donor_zips(raw_data),
-  donation_zips_csv = write_csv(donor_zips_df, file_out("data/donor_zips.csv")),
+  donation_zips_csv = write_csv(donor_zips_df, here(file_out("data/donor_zips.csv"))),
 )
 
 load_plan <- drake_plan(
@@ -61,14 +61,15 @@ load_plan <- drake_plan(
 correct_plan <- drake_plan(
   employer_pattern_df = read_csv(here(file_in("data/employer_replacements.csv"))),
   employer_pattern_replacements_df = simplify_companies(raw_data, employer_pattern_df),
-  employer_pattern_replacements_csv = write_csv(employer_pattern_replacements_df, file_out("data/employer_pattern_replacements.csv")),
+  employer_pattern_replacements_csv = write_csv(employer_pattern_replacements_df, here(file_out("data/employer_pattern_replacements.csv"))),
   loaded_employer_pattern_replacements = merge_companies(con, here(file_in("data/employer_pattern_replacements.csv"))),
   corrected_employer_names = target(neo4r::call_neo4j("MATCH (employer: Employer) RETURN DISTINCT employer.name", con = con, type = "row")$employer.name %>% transmute(employer_name=value), trigger = trigger(change=loaded_employer_pattern_replacements)),
   industry_taxonomy_df = read_csv(here(file_in("data/industry_taxonomy.csv"))),
   industry_pattern_df = read_csv(here(file_in("data/industry_mapping_patterns.csv")), col_types = list(col_character(), col_factor(industry_taxonomy_df$name), col_character())),
   industry_pattern_replacements = label_companies(industry_pattern_df, corrected_employer_names),
-  industry_pattern_replacements_csv= write_csv(industry_pattern_replacements, file_out("data/industry_pattern_replacements.csv")),
-  unlabeled_companies = corrected_employer_names %>% dplyr::filter(!employer_name %in% industry_pattern_replacements$employer_name),
+  industry_pattern_replacements_csv = write_csv(industry_pattern_replacements, here(file_out("data/industry_pattern_replacements.csv"))),
+  unlabeled_companies_df = corrected_employer_names %>% dplyr::filter(!employer_name %in% industry_pattern_replacements$employer_name),
+  unlabeled_companies_csv = write_csv(corrected_employer_names, here(file_out("data/unlabeled_companies.csv"))),
 )
 
 plan <- bind_rows(raw_data_plan, extract_entities_plan, extract_relationships_plan, load_plan, correct_plan)
--- data/schema.cypher
+++ data/schema.cypher
@@ -5,3 +5,8 @@ CREATE CONSTRAINT ON (donor:Donor) ASSERT donor.name IS UNIQUE;
 
 CREATE CONSTRAINT ON (employer:Employer) ASSERT employer.name IS UNIQUE;
 
+CREATE CONSTRAINT ON (zipcode:ZipCode) ASSERT zipcode.zip_code IS UNIQUE;
+
+CREATE CONSTRAINT ON (industry:Industry) ASSERT industry.name IS UNIQUE;
+
+CREATE CONSTRAINT ON (state:State) ASSERT state.abbreviation IS UNIQUE;
