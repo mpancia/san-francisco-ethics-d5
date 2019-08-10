7e85e0adc434aa4357ea8e0cc75c69979e78572c
Matthew Pancia <msp@themsp.org>
2019-08-09 21:29:40 -0700

    A bit of refactoring.

--- R/functions.R
+++ R/functions.R
@@ -32,22 +32,34 @@ apply_labels_to_dataframe <-
              match_dataframe,
              match_column_name,
              match_label_column_name) {
-    pattern_column <- sym(pattern_column_name)
-    pattern_label_column <- sym(pattern_label_column_name)
-    match_column <- sym(match_column_name)
-    match_label_column <- sym(match_label_column_name)
+    # Convert the column names to symbols
+    c(
+      pattern_column,
+      pattern_label_column,
+      match_column,
+      match_label_column
+    ) %<-% map(list(
+      pattern_column_name,
+      pattern_label_column_name,
+      match_column_name,
+      match_label_column_name
+    ), sym)
 
     apply_row_label <- function(current_state, row) {
-      with(row, {
-        pattern <- eval(pattern_column)
-        label <- as.character(eval(pattern_label_column))
-        mutate_expr <- quo(ifelse(
-          str_detect(!!match_column, !!pattern), !!label,
-          !!match_label_column
-        ))
-        current_state %>% mutate(!!match_label_column := !!mutate_expr)
-      })
+      # Evaluate the pattern and label for the current row, converting the label to a character if necessary
+      c(pattern, label) %<-% map(
+        list(pattern_column, pattern_label_column),
+        function(x) as.character(eval(x, envir = row))
+      )
+      # Make an expression that matches on the label and replaces it
+      mutate_expr <- quo(ifelse(
+        str_detect(!!match_column, !!pattern), !!label,
+        !!match_label_column
+      ))
+      # Apply the expression to create a new column
+      current_state %>%
+        mutate(!!match_label_column := !!mutate_expr)
     }
-
+    # Convert the dataframe to a list of rows, then reduce, starting with the original input match dataframe.
     reduce(pmap(pattern_dataframe, list), apply_row_label, .init = match_dataframe)
   }
