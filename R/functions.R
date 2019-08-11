abbe104002e9057fdd9b5cb86f2e372a2a5544f1
Matthew Pancia <msp@themsp.org>
2019-08-10 22:01:02 -0700

    Refactoring.

--- R/functions.R
+++ R/functions.R
@@ -63,3 +63,52 @@ apply_labels_to_dataframe <-
     # Convert the dataframe to a list of rows, then reduce, starting with the original input match dataframe.
     reduce(pmap(pattern_dataframe, list), apply_row_label, .init = match_dataframe)
   }
+
+#' Apply regex substitutions  to a dataframe using a dataframe of patterns/substitutions.
+#'
+#' @param pattern_dataframe An input dataframe containing patterns
+#' @param pattern_column_name The name of the column in the pattern dataframe containing the patterns
+#' @param pattern_substitution_column_name The name of the column in the pattern dataframe containing the regex substitutions
+#' @param match_dataframe An input dataframe containing a character column to pattern match on
+#' @param match_column_name The name of the column to match the patterns on
+#' @param match_substitution_column_name The name of the resulting substitution column
+#'
+#' @return A dataframe that looks like \code{match dataframe}.
+#' @export
+#'
+#' @examples
+apply_substitution_to_dataframe <-
+  function(pattern_dataframe,
+             pattern_column_name,
+             pattern_substitution_column_name,
+             match_dataframe,
+             match_column_name,
+             match_substitution_column_name) {
+    # Convert the column names to symbols
+    c(
+      pattern_column,
+      pattern_substitution_column,
+      match_column,
+      match_substitution_column
+    ) %<-% map(list(
+      pattern_column_name,
+      pattern_substitution_column_name,
+      match_column_name,
+      match_substitution_column_name
+    ), sym)
+
+    apply_row_replacement <- function(current_state, row) {
+      # Evaluate the pattern
+      c(pattern, replacement) %<-% map(
+        list(pattern_column, pattern_substitution_column),
+        function(x) eval(x, envir = row)
+      )
+      # Make an expression that matches on the pattern and does the substitution
+      mutate_expr <- quo(str_replace_all(!!match_column, !!pattern, !!replacement))
+      # Apply the expression to create a new column
+      current_state %>%
+        mutate(!!match_substitution_column := !!mutate_expr)
+    }
+    # Convert the dataframe to a list of rows, then reduce, starting with the original input match dataframe.
+    reduce(pmap(pattern_dataframe, list), apply_row_replacement, .init = match_dataframe)
+  }
